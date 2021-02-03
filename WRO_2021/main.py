a7b15873b46ab1816370adc33c8ca4fcae137538
SchwammPizza <ambros.anrig@gmail.com>
2021-02-03 17:55:22 +0100

    main

--- WRO_2021/main.py
+++ WRO_2021/main.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env pybricks-micropython
 from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
-                                 InfraredSensor, UltrasonicSensor, GyroSensor)
-from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
