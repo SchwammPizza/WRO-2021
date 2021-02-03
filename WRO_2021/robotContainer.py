b20779e79cf9944e4ff1b94b49fabaaa157ae4d0
Cortus <alicia.eugster5@gmail.com>
2021-02-03 17:55:49 +0100

    robotContainer

---
+++ WRO_2021/robotContainer.py
@@ -0,0 +1,10 @@
+class robotContainer:
+    wheel_diameter = 5.5
+    wheel_distance = 18.7
+    speed = 50
+    fast_speed = 70
+    slow_speed = 30
+    approach_speed = 20
+    turn_speed = 20
+    slowturn_speed = 10
+    line = ["black"] 
