678cae0eec993f54be1ed4bea7e68e50dffa0c96
SchwammPizza <ambros.anrig@gmail.com>
2021-02-04 16:51:51 +0100

    Auto stash before merge of "main" and "origin/main"

--- WRO_2021/main.py
+++ WRO_2021/main.py
@@ -8,21 +8,15 @@ from motors import motor
 from robotContainer import robotContainer as RC
 from driveTrain import driveTrain
 from lifter import Lifter
+from Gripper import Gripper
 
+gripper = Gripper()
 lifter = Lifter()
 DriveTrain = driveTrain()
 Motor = motor()
 rc = RC()
 
 
-
-# This program requires LEGO EV3 MicroPython v2.0 or higher.
-# Click "Open user guide" on the EV3 extension tab for more information.
-
-
-# Create your objects here.
 ev3 = EV3Brick()
 
-
-# Write your program here.
 
