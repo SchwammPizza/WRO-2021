6767d1d40d408bf85a80a84a3991e3bcd0ed7cc7
Cortus <alicia.eugster5@gmail.com>
2021-03-09 17:03:40 +0100

    checkpoint2 and gripper

---
+++ WRO_2021/RobotArm.py
@@ -0,0 +1,32 @@
+from Gripper import Gripper, GripperPositions
+from lifter import Lifter, LifterPositions
+
+class InstanceBuffer:
+    instance = 0
+
+class RobotArm:
+    def __init__(self):
+        self.gripper = Gripper.getInstance()
+        self.lifter = Lifter.getInstance()
+        self.resetRobotArm()
+
+    @staticmethod
+    def getInstance():
+        if (InstanceBuffer.instance == 0):
+            instance = RobotArm()
+        return instance
+
+    def resetRobotArm(self):
+        self.lifter.resetPosition()
+        self.gripper.openGripper()
+        self.gripperPosition = 0
+        self.lifterPosition = 0
+
+    def moveToGrippingPosition(self):
+        if self.gripperPosition == 1:
+            self.gripper.openGripper()
+        if self.lifterPosition == 0:
+            self.lifter.moveMotor(100, 100)
+        self.lifterPosition = 1
+        self.gripperPosition = 0
+
