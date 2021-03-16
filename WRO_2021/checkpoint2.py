5f83a70dd1df3776472a374e656a00e2ccc1b840
Cortus <alicia.eugster5@gmail.com>
2021-03-16 19:38:18 +0100

    YellowAGripper

--- WRO_2021/checkpoint2.py
+++ WRO_2021/checkpoint2.py
@@ -12,23 +12,37 @@ class Checkpoint2:
     def __init__(self):
         pass
     
-    def gripA(self, speed, turn_speed, dist, left, right):
-        speed *= 10
-        turn_speed *= 10
+    def YellowAGripper(self, direction):
+        #direction is a boolean
         dist = 33
         dist2 = 41.6
         left = 90
         right = -90
 
+        if direction == "left":
+            turn1 = left
+            turn2 = right
+
+        elif direction == "right":
+            turn2 = left
+            turn1 = right
+            
+
         lifter.runTrue(-RC.slow_speed)
-        DriveTrain.turnOnPoint(left, RC.turn_speed)
+        DriveTrain.turnOnPoint(turn1, RC.turn_speed)
         DriveTrain.driveForward(dist, RC.speed)
-        # DriveTrain.turnOnPoint(right, rc.turn_speed)
-        # lifter.moveMotor(rc.speed, right)
-        # gripper.moveMotor(self, rc.speed, right)
-        # lifter.moveMotor(self, rc.speed, left)
-        # DriveTrain.turnOnPoint(self, right, rc.turn_speed)
-        # DriveTrain.forward(self, dist, rc.speed)
-        # DriveTrain.turnOnPoint(self, right, rc.turn_speed)
-        # DriveTrain.forward(self, dist2, rc.speed)
+        DriveTrain.turnOnPoint(turn2, RC.turn_speed)
+
+        #nächste 3 zeilen ersetzen (Befehle Julian) => Grippen
+        lifter.moveMotor(RC.speed, right)
+        gripper.moveMotor(RC.speed, right)
+        lifter.moveMotor(RC.speed, left)
+
+        DriveTrain.turnOnPoint(turn2, RC.turn_speed)
+        DriveTrain.driveForward(dist, RC.speed)
+        DriveTrain.turnOnPoint(turn2, RC.turn_speed)
+        DriveTrain.driveForward(dist2, RC.speed)
         #start von 2 grip gelbe AA bei 2.2, schaut nach unten
+
+#yellowAGripper name ändern, right left drive
+#turn1 und turn 2 gegengleich, je nachdem ob rechts oder links
