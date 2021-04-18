56422c8e2464e8b08d42dbc918f830b3b578dd6d
Unknown <florian.helbling@outlook.com>
2021-04-18 16:31:47 +0200

    finished pickupgreenA

--- WRO_2021/PickupGreenA.py
+++ WRO_2021/PickupGreenA.py
@@ -26,11 +26,11 @@ class pickupGreenA:
 
     #side in left or right
     def driveToPickupDestination(self, side):
-        self.driveTrain.turnOnWheel(90, -robotContainer.turnOnWheel_speed, self.switchSide(side))
-        self.driveTrain.turnOnPoint(89 * ((side == "right") * 2 - 1), robotContainer.turn_speed)
+        self.driveTrain.turnOnWheel(90, -robotContainer.turnOnWheel_speed, side)
+        self.driveTrain.turnOnPoint(89 * ((side == "left") * 2 - 1), robotContainer.turn_speed)
         self.robotArm.moveToPickupAPosition()
         self.driveTrain.driveForward(13, robotContainer.getInstance().slow_speed)
         self.robotArm.moveUp()
-        self.driveTrain.turnOnWheel(90, -robotContainer.turnOnWheel_speed, side)
-        self.driveTrain.turnOnPoint(90 * ((side == "left") * 2 - 1), robotContainer.turn_speed)
-        self.driveTrain.driveForward(4, robotContainer.getInstance().slow_speed)
+        self.driveTrain.turnOnWheel(90, -robotContainer.turnOnWheel_speed, self.switchSide(side))
+        self.driveTrain.turnOnPoint(88 * ((side == "right") * 2 - 1), robotContainer.turn_speed)
+        self.driveTrain.driveForward(4.5, robotContainer.getInstance().slow_speed)
