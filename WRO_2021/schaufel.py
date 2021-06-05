9284f255360252da0b0f861c4a02fec482cda8ec
Cortus <alicia.eugster5@gmail.com>
2021-06-05 14:05:14 +0200

    schaufel

--- WRO_2021/schaufel.py
+++ WRO_2021/schaufel.py
@@ -14,17 +14,26 @@ class schaufel():
     def __init__(self) -> None:
         pass
 
+    def schnappA():
+        lifter.moveUp()
+        lifter.moveMotor(RC.speed, -50)
+
+    def battery_heben():
+        
 
 
-    def grabGreenA(self):
-        # drive to greens 
+    def grabBlueA(self):
+        # drive to blue 
         # => optimieren
-        lifter.moveMotor(RC.lifterSpeed, #angel)
+        DT.turnOnPoint(-90, RC.speed)
+        DT.driveForward(-18, RC.speed)
+        lifter.moveMotor(RC.lifterSpeed, #angel) => runter
         DT.driveForward(#dist, RC.speed)
         lifter.moveMotor(RC.lifterSpeed, #angle)
         DT.driveForward(#dist, -RC.speed)
         #evtl oben lassen falls irgendwo reintun
         #sonst:
         #mehr zurückfahren um abladen+einnehmen
-        lifter.moveMotor(RC.)
+        #lifter.moveMotor()
 
+    
