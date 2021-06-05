d9dedce4b95ca644a11a5f4a1d0170986434bcc3
Cortus <alicia.eugster5@gmail.com>
2021-06-05 14:16:36 +0200

    asldhgajkb

--- WRO_2021/schaufel.py
+++ WRO_2021/schaufel.py
@@ -12,28 +12,34 @@ RC = rc()
 
 class schaufel():
     def __init__(self) -> None:
-        pass
+        lifter.moveMotor(RC.speed, -180)
 
-    def schnappA():
-        lifter.moveUp()
-        lifter.moveMotor(RC.speed, -50)
+    @staticmethod
+    def schnapp():
+        lifter.moveMotor(RC.speed, 90)
+    
+    @staticmethod
+    def entschnapp():
+        lifter.moveMotor(RC.speed, -90)
+        Motor.VorneMotor.VorneMotor.hold()
 
     def battery_heben():
-        
+        DT.driveForward()
+        lifter.moveMotor(RC.speed, 10)
+        DT.driveForward()
 
 
     def grabBlueA(self):
+        pass
         # drive to blue 
         # => optimieren
-        DT.turnOnPoint(-90, RC.speed)
-        DT.driveForward(-18, RC.speed)
-        lifter.moveMotor(RC.lifterSpeed, #angel) => runter
-        DT.driveForward(#dist, RC.speed)
-        lifter.moveMotor(RC.lifterSpeed, #angle)
-        DT.driveForward(#dist, -RC.speed)
+        # DT.turnOnPoint(-90, RC.speed)
+        # DT.driveForward(-18, RC.speed)
+        # lifter.moveMotor(RC.lifterSpeed, #angel) => runter
+        # DT.driveForward(#dist, RC.speed)
+        # lifter.moveMotor(RC.lifterSpeed, #angle)
+        # DT.driveForward(#dist, -RC.speed)
         #evtl oben lassen falls irgendwo reintun
         #sonst:
         #mehr zurückfahren um abladen+einnehmen
         #lifter.moveMotor()
-
-    
