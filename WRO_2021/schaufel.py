7e6cfdf2a927d1dcfbfeb4f6e9050bf5de7ff4f6
Cortus <alicia.eugster5@gmail.com>
2021-06-05 15:21:56 +0200

    solar +

--- WRO_2021/schaufel.py
+++ WRO_2021/schaufel.py
@@ -35,18 +35,35 @@ class schaufel():
         DT.driveForward(20, RC.speed)
         DT.driveForward(7, RC.speed)
 
+    def solars(self):
+        #start cp 2
+        DT.turnOnPoint(-90, RC.speed)
+        DT.driveForward(20, RC.speed)
+        DT.turnOnWheel(180, RC.turn_speed, "left")
+        DT.driveForward(5, RC.speed)
+        self.schnapp()
+        DT.driveForward(70, RC.speed)
+        self.entschnapp()
+
+
 
     def grabBlueA(self):
         pass
         # drive to blue 
         # => optimieren
+
         # DT.turnOnPoint(-90, RC.speed)
         # DT.driveForward(-18, RC.speed)
-        # lifter.moveMotor(RC.lifterSpeed, #angel) => runter
-        # DT.driveForward(#dist, RC.speed)
-        # lifter.moveMotor(RC.lifterSpeed, #angle)
-        # DT.driveForward(#dist, -RC.speed)
-        #evtl oben lassen falls irgendwo reintun
-        #sonst:
-        #mehr zurückfahren um abladen+einnehmen
-        #lifter.moveMotor()
+        # lifter.moveMotor(-RC.lifterSpeed, 75)
+        # DT.driveForward(6, RC.speed)
+        # lifter.moveMotor(RC.lifterSpeed, 50) 
+        # DT.driveForward(12, RC.speed)
+        # DT.turnOnPoint(90, RC.turn_speed)
+        # DT.driveForward(17, RC.speed)
+        # DT.turnOnPoint(-90, RC.speed)
+        # DT.driveForward(-18, RC.speed)
+
+        # evtl oben lassen falls irgendwo reintun
+        # sonst:
+        # mehr zurückfahren um abladen+einnehmen
+        # lifter.moveMotor()
